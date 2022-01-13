981430c8538492d7e0a320b16ab3b104e22e2afd
ujin-Bang <qkddnwls14@gmail.com>
2022-01-13 15:45:54 +0900

    실제로 로그인 API 호출 세팅  - 서버 접속 기능은 모든 화면에서 필요로 함.  - BaseActivity에 멤버 변수로 만들어서 상속

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/BaseActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/BaseActivity.kt
@@ -3,15 +3,22 @@ package com.neppplus.retrofitlibrarytest_20220113
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.neppplus.retrofitlibrarytest_20220113.api.ServerAPI
+import com.neppplus.retrofitlibrarytest_20220113.api.ServerAPIService
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext: Context
 
+    lateinit var apiService : ServerAPIService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        val retrofit = ServerAPI.getRetrofit()
+        apiService = retrofit.create(ServerAPIService::class.java)
     }
     abstract fun setupEvents()
     abstract fun setValues()
