a5c885b1add07f43ce3ef37f42daeb08ebb4be70
ujin-Bang <qkddnwls14@gmail.com>
2022-01-13 14:35:13 +0900

    BaseActivity 기본 작업

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/MainActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarytest_20220113.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
