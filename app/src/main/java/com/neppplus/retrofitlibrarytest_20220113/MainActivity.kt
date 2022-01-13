9587e24c475faa60b75c5d71d93802bd3842a336
ujin-Bang <qkddnwls14@gmail.com>
2022-01-13 14:25:11 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.neppplus.retrofitlibrarytest_20220113
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.retrofitlibrarytest_20220113.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
