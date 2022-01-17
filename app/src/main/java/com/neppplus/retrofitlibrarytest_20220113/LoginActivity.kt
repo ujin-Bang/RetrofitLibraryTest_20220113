326d9c4a3735956cb75abc9f323f615a1ba92b71
ujin-Bang <qkddnwls14@gmail.com>
2022-01-17 13:22:15 +0900

    화면 이름 변경 / 서버 주소 원상복구

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/LoginActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/LoginActivity.kt
@@ -1,24 +1,23 @@
 package com.neppplus.retrofitlibrarytest_20220113
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.neppplus.retrofitlibrarytest_20220113.databinding.ActivityMainBinding
+import com.neppplus.retrofitlibrarytest_20220113.databinding.ActivityLoginBinding
 import com.neppplus.retrofitlibrarytest_20220113.datas.BasicResponse
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+       binding= DataBindingUtil.setContentView(this,R.layout.activity_login)
         setupEvents()
         setValues()
     }
