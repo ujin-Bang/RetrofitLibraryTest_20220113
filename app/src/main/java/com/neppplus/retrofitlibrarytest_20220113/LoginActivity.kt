f8249089dd8f651099dfd2900c2c02ded3525881
ujin-Bang <qkddnwls14@gmail.com>
2022-01-17 13:41:51 +0900

    #1 로그인 성공시 문구 변경

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/LoginActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/LoginActivity.kt
@@ -49,7 +49,7 @@ class LoginActivity : BaseActivity() {
 //                      추가 파싱 -> 로그인한 사람의 닉네임 활용 "~님 환영합니다!" 토스트
                         val userNickname = basicResponse.data.user.nickname
 
-                        Toast.makeText(mContext, "${userNickname}님 환영합니다.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(mContext, "${userNickname}님 환영합니다!", Toast.LENGTH_SHORT).show()
                     }
                    else {
                        val errorJson = JSONObject(response.errorBody()!!.string())
