c6276b3fa36702b43389345ed5e67c6f2404cd33
ujin-Bang <qkddnwls14@gmail.com>
2022-01-18 13:05:53 +0900

    리뷰 목록 프래그먼트 추가  - 뷰페이져 추가 중( 필요 프래그먼트 제작)

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/fragments/ReviewListFragment.kt
@@ -0,0 +1,24 @@
+package com.neppplus.retrofitlibrarytest_20220113.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.retrofitlibrarytest_20220113.R
+
+class ReviewListFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_review_list,container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
