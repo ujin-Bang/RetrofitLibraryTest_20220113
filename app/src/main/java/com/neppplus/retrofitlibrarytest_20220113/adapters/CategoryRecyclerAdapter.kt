29a4df66d9f44506ba789abb8c35afb7b958383c
ujin-Bang <qkddnwls14@gmail.com>
2022-01-18 16:01:11 +0900

    리싸이클러뷰 활용 연습  4) 어댑터 뼈대 작업 - 2

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/adapters/CategoryRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/adapters/CategoryRecyclerAdapter.kt
@@ -1,16 +1,35 @@
 package com.neppplus.retrofitlibrarytest_20220113.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.retrofitlibrarytest_20220113.R
+import com.neppplus.retrofitlibrarytest_20220113.datas.SmallCategoryData
 
-class CategoryRecyclerAdapter: RecyclerView.Adapter<CategoryRecyclerAdapter.CategoryViewHolder>() {
+class CategoryRecyclerAdapter(
+    val mContext: Context,
+    val mList: List<SmallCategoryData>
+    ): RecyclerView.Adapter<CategoryRecyclerAdapter.CategoryViewHolder>() {
 
 //    클래스 내부의 또 다른 클래스(inner class)
     inner class CategoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-
+//  이 내부의 용도: xml -> UI 찾아내서 -> 데이터 반영 기능
 
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+
+     val row = LayoutInflater.from(mContext).inflate(R.layout.category_list_item, parent, false)
+     return CategoryViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
 
 }
