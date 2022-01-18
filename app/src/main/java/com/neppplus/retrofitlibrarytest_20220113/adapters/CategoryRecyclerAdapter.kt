8497804ef4669e8b9c40204d271851bee91723da
ujin-Bang <qkddnwls14@gmail.com>
2022-01-18 15:43:19 +0900

    리싸이클러뷰 활용 연습  4) 어댑터 뼈대 작업 - 1

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/adapters/CategoryRecyclerAdapter.kt
@@ -0,0 +1,16 @@
+package com.neppplus.retrofitlibrarytest_20220113.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoryRecyclerAdapter: RecyclerView.Adapter<CategoryRecyclerAdapter.CategoryViewHolder>() {
+
+//    클래스 내부의 또 다른 클래스(inner class)
+    inner class CategoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+
+
+    }
+
+
+}
