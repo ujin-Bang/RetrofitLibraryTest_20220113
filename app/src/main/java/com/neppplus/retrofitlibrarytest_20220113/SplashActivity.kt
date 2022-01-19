34b4c8b55902802ab34a130830bcd00291b81250
ujin-Bang <qkddnwls14@gmail.com>
2022-01-19 21:30:45 +0900

    자동 로그인 검사기준 변경  - 저장된 토큰으로 내 정보 불러내보기  - 성공적으로 불러와 져야만 자동 로그인 통과

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/SplashActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/SplashActivity.kt
@@ -7,6 +7,11 @@ import android.os.Handler
 import android.os.Looper
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarytest_20220113.databinding.ActivitySplashBinding
+import com.neppplus.retrofitlibrarytest_20220113.datas.BasicResponse
+import com.neppplus.retrofitlibrarytest_20220113.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
 
@@ -24,12 +29,29 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
 
+        var loginUser: UserData? = null
+
+        apiService.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object :Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if(response.isSuccessful){
+//                   //올바른 토큰일 때 -> loginUser에 파싱해서 객체 대입
+                     loginUser = response.body()!!.data.user
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
         val myHandler = Handler(Looper.getMainLooper())
 
         myHandler.postDelayed({
             val myIntent: Intent
 
-            if( ContextUtil.getToken(mContext) !="" ) {
+            if( loginUser != null ) {
                 myIntent = Intent(mContext, MainActivity::class.java)
 
             }
