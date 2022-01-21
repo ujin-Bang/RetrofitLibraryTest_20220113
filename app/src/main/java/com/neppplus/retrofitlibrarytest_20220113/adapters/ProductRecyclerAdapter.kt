53a1d2386361e71ec210cda356dce2107557a109
ujin-Bang <qkddnwls14@gmail.com>
2022-01-21 12:50:13 +0900

    LongClick시 경고차 띄우기(예제)

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/adapters/ProductRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/adapters/ProductRecyclerAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.neppplus.retrofitlibrarytest_20220113.R
@@ -39,7 +40,18 @@ class ProductRecyclerAdapter(val mContext: Context, val mList: List<ProductData>
 
                 Toast.makeText(mContext, "${data.name}상품이 클릭됨", Toast.LENGTH_SHORT).show()
 
+            }
 
+            rootLayout.setOnLongClickListener {
+
+                val alert = AlertDialog.Builder(mContext)
+                alert.setTitle("상품 삭제")
+                alert.setMessage("정말 해당 상품을 삭제하시겠습니까?")
+                alert.setPositiveButton("확인",null)
+                alert.setNegativeButton("취소",null)
+                alert.show()
+
+                return@setOnLongClickListener true
             }
 
 
