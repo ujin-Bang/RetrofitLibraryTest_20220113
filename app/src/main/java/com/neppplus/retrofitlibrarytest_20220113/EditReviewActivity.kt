25a49e52426ed279eb4d35d56af93b2efe4f87e9
ujin-Bang <qkddnwls14@gmail.com>
2022-01-23 20:38:14 +0900

    리뷰 작성화면 데이터 표시 작업중 - 넘겨받은 상품 정보 - GlobalData에 세팅한 사용자 정보

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/EditReviewActivity.kt
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarytest_20220113.databinding.ActivityEditReviewBinding
+import com.neppplus.retrofitlibrarytest_20220113.datas.ProductData
+import com.neppplus.retrofitlibrarytest_20220113.utils.GlobalData
 
 class EditReviewActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditReviewBinding
+
+    lateinit var mPorductData: ProductData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        binding= DataBindingUtil.setContentView(this,R.layout.activity_edit_review)
@@ -17,9 +22,16 @@ class EditReviewActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+
+
     }
 
     override fun setValues() {
+
+        mPorductData = intent.getSerializableExtra("product") as ProductData
+
+        binding.txtProductName.text = mPorductData.name
+        binding.txtUserNickname.text = GlobalData.loginUser!!.nickname
 
     }
 }
