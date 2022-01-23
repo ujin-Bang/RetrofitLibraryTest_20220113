865095179798e635a44cbc83eb7e665bba55221e
ujin-Bang <qkddnwls14@gmail.com>
2022-01-23 22:59:01 +0900

    리뷰 데이터 파싱용 클래스 추가  - 리싸이클러뷰의 데이터 클래스로도 활용

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/datas/ReviewData.kt
@@ -0,0 +1,15 @@
+package com.neppplus.retrofitlibrarytest_20220113.datas
+
+import java.io.Serializable
+
+class ReviewData(
+
+    var id: Int,
+    var title: String,
+    var content: String,
+    var score: Double,
+    var user: UserData,
+    var product: ProductData,
+
+): Serializable {
+}
