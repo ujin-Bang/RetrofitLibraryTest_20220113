33785bd15c7057a4c12255eb6001b338338c776c
ujin-Bang <qkddnwls14@gmail.com>
2022-01-24 16:07:47 +0900

    태그 입력 기능  - 띄어쓰기시 태그로 등록/ 입력칸 초기화

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/EditReviewActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.retrofitlibrarytest_20220113
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarytest_20220113.databinding.ActivityEditReviewBinding
 import com.neppplus.retrofitlibrarytest_20220113.datas.BasicResponse
@@ -13,12 +14,15 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class EditReviewActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditReviewBinding
 
     lateinit var mPorductData: ProductData
+
+    val mInputTagList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +33,40 @@ class EditReviewActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+//         한글자 입력할 때마다 -> 스페이스를 넣었는지 검사.
+        binding.edtTag.addTextChangedListener {
+
+            val nowText = it.toString()
+
+            if(nowText =="") {
+//                빈칸일 때는 밑의 코드 실행 X
+                return@addTextChangedListener
+            }
+            Log.d("입력값",it.toString())
+
+//            지금 입력된 내용의 마지막 글자가 ' ' 인가?
+           if( nowText.last() ==' ') {
+               Log.d("입력값","스페이스가 들어옴")
+
+           //               입력된 값을 태그로 등록
+
+//               태그로 등록될 문구 => " "공백 제거
+             val tag = nowText.replace(" ","")
+
+//               태그 목록으로 추가해 보자.
+               mInputTagList.add( tag )
+
+//               입력값 초기화
+               binding.edtTag.setText("")
+           }
+
+        }
+
     binding.btnWrite.setOnClickListener {
+
+        for(tag in mInputTagList) {
+            Log.d("입력태그", tag)
+        }
 
         val inputTitle = binding.edtReviewTitle.text.toString()
         val inputContent = binding.edtContent.text.toString()
