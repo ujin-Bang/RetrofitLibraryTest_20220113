4cfe28c7f32e89cb06eee73fd2c14d24d79c8572
ujin-Bang <qkddnwls14@gmail.com>
2022-01-25 20:28:12 +0900

    ViewType 개념 활용  - 어느 위치에, 어떤 모양을 그려야 하는지 표식을 Int로 남겨주자

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/adapters/MainRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/adapters/MainRecyclerAdapter.kt
@@ -1,9 +1,12 @@
 package com.neppplus.retrofitlibrarytest_20220113.adapters
 
+import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.retrofitlibrarytest_20220113.datas.ReviewData
 
-class MainRecyclerAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MainRecyclerAdapter(val mContext: Context, val mList: List<ReviewData>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 //    2가지 ViewHolder 필요함 => 0번칸: 상단부(Header) xml / 나머지(Item)칸: 리뷰모양 xml
 
@@ -15,5 +18,32 @@ class MainRecyclerAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     inner class ItemViewHolder(row: View): RecyclerView.ViewHolder(row){
 
     }
+
+//    position별로 어떤 모양이 나가야 하는지(viewType이 어떻게 되는지 ) 알려줄 함수
+
+    val HEADER_VIEW_TYPE = 1000
+    val REVIEW_ITEM_TYPE = 1001
+
+    override fun getItemViewType(position: Int): Int {
+
+//        position 0: 맨 윗칸 => 상단 뷰
+//        position 그 외: 목록 표시. => 리뷰 아이템
+
+        return when(position ) {
+
+            0 -> HEADER_VIEW_TYPE
+                else -> REVIEW_ITEM_TYPE
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size +1
 
 }
