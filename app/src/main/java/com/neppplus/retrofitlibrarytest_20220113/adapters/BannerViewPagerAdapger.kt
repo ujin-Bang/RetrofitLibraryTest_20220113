c76d5735a5b554d9c73aabe5e6281d72287c7432
ujin-Bang <qkddnwls14@gmail.com>
2022-01-26 16:12:27 +0900

    배너 뷰페이저 어댑터 작성 - 어떤 배너 목록을 보여줄지 받아오자 - 페이지별 프래그먼트가 전부 동일(BannerFragment)  => 각 페이지별로 어떤 배너를 보여줄지 생성자에 담아준다.

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20220113/adapters/BannerViewPagerAdapger.kt
@@ -0,0 +1,17 @@
+package com.neppplus.retrofitlibrarytest_20220113.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.retrofitlibrarytest_20220113.datas.BannerData
+import com.neppplus.retrofitlibrarytest_20220113.fragments.BannerFragment
+
+class BannerViewPagerAdapger(fm: FragmentManager, val mBannerList: List<BannerData>): FragmentPagerAdapter(fm) {
+    override fun getCount()= mBannerList.size
+
+    override fun getItem(position: Int): Fragment {
+
+        return BannerFragment(mBannerList[position])
+
+    }
+}
